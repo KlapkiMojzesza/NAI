f27f2b21f7739a11341c3417ae22c72506a3049e
KlapkiMojzesza <77103823+KlapkiMojzesza@users.noreply.github.com>
2022-12-07 15:37:21 +0100

    lab07

---
+++ lab07.cpp
@@ -0,0 +1,25 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    using namespace cv;
+    VideoCapture vc(0);
+    if (!vc.isOpened() ) return 1;
+    cv::Mat frame;
+
+    cv::namedWindow("Window", WINDOW_AUTOSIZE); // Create a window for display.
+    int i = 0;
+    while (waitKey(10) != 27) {
+        vc >> frame;
+
+        cv::Mat src= frame;
+        cv::Mat dst;
+        cv::flip(src, dst, 1);
+
+        cv::imshow("Frame", dst);
+
+    }
+    return 0;
+}
